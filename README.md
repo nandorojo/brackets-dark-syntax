790ef1939b24151f375463e462fd2faee155497b
Sebastian Thaler <sebi.thaler@gmail.com>
2018-05-09 14:06:38 +0200

    Javascript Update
    Update Javascript Syntax Highlighting and some optional Files

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # brackets-dark-syntax
-### A syntax highlighting theme for the Atom Editor
 
 This theme was inspired by the Brackets Editor's default syntax highlighting and is built with web development in mind.
 
+## Preview
+
 ![Screenshot](sample.png)
+
+## Install
+
+### With Atom
+1. Open **Settings > Install**
+2. Set the filter to **Themes**
+3. Search for `brackets-dark-syntax` and install
+
+### With the CLI
+```shell
+apm install brackets-dark-syntax
+```
---
+++ file-samples/javascript.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    "use strict";
+
+    // --------------------------------
+    // Scroll to Anchor
+    // --------------------------------
+
+    $('a[href*=\\#]:not([href=\\#])').bind('click', function (event) {
+        if ($(this).attr('href').toLowerCase().indexOf('#') >= 0) {
+            event.preventDefault();
+
+            // setTimeout -> foundation framework fix
+            // if link is in menu on mobile -> wait till menu is closed
+            setTimeout($.proxy(function () {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top
+                }, 1500, 'easeInOutExpo');
+            }, this), 50);
+        }
+    });
+
+    // --------------------------------
+    // Scroll Reveal
+    // --------------------------------
+
+    // Changing the defaults
+    window.sr = ScrollReveal({
+        origin: 'bottom',
+        distance: '10px',
+        duration: 500,
+        delay: 0,
+        opacity: 0,
+        scale: 0.9,
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        mobile: false,
+        viewFactor: 0.2,
+    });
+
+	 $('.co-projects .project').each(function(index) {
+		 var className = 'proj-' + index;
+		 var revealName = '.co-projects .' + className + ' .text .tag';
+
+		 $(this).addClass(className);
+
+		 sr.reveal(revealName, delay: 200, 300);
+	 });
+
+});
